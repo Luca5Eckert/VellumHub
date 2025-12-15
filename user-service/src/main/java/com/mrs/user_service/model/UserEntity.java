a0d10fd1bc02f8a8b7e7766ebb280699c6e8c135
Luca5Eckert <ecklukatv@gmail.com>
2025-12-15 19:17:50 -0300

    refactor: adiciona anotação para hibernate entender delete por atributo

--- user-service/src/main/java/com/mrs/user_service/model/UserEntity.java
+++ user-service/src/main/java/com/mrs/user_service/model/UserEntity.java
@@ -1,35 +1,49 @@
 package com.mrs.user_service.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_users")
-@Data
+@SQLDelete(sql = "UPDATE tb_users SET deleted_at = CURRENT_TIMESTAMP, active = false WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
+    @UuidGenerator
     private UUID id;
 
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Email(message = "O email deve ser válido")
+    @NotBlank(message = "O email é obrigatório")
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
     private String password;
-
-    private String fullName;
 
     @Builder.Default
     private boolean active = true;
@@ -38,12 +52,26 @@ public class UserEntity {
     private Long version;
 
     @CreationTimestamp
-    @Column(updatable = false)
-    private Instant createAt;
+    @Column(updatable = false, name = "created_at")
+    private Instant createdAt;
 
     @UpdateTimestamp
-    private Instant updateAt;
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
+    @Column(name = "deleted_at")
     private Instant deletedAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserEntity that = (UserEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
