ebbb005a0cc15c9d29c0e2a802c5db7698a73fa3
Luca5Eckert <ecklukatv@gmail.com>
2025-12-22 22:23:27 -0300

    feat: adiciona classe wrapper para busca na api

---
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/dto/RecommendatioResponse.java
@@ -0,0 +1,13 @@
+package com.mrs.recommendation_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mrs.recommendation_service.model.Recommendation;
+
+import java.util.List;
+import java.util.UUID;
+
+public record RecommendationResponse(
+        @JsonProperty("user_id") UUID userId,
+        List<Recommendation> recommendations,
+        Integer count
+) {}
