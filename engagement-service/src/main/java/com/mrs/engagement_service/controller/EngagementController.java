7aa1dae6824444b2680b1ed7704747050e2485d1
Luca5Eckert <ecklukatv@gmail.com>
2026-01-15 10:51:15 -0300

    #28 feat(engagement-service): adiciona endpoint para pegar interações do usuário

--- engagement-service/src/main/java/com/mrs/engagement_service/controller/EngagementController.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/controller/EngagementController.java
@@ -1,14 +1,17 @@
 package com.mrs.engagement_service.controller;
 
 import com.mrs.engagement_service.dto.InteractionCreateRequest;
+import com.mrs.engagement_service.dto.InteractionGetResponse;
+import com.mrs.engagement_service.model.InteractionType;
 import com.mrs.engagement_service.service.EngagementService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/engagement")
@@ -25,6 +28,22 @@ public class EngagementController {
         engagementService.create(engagement);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Engagement registered with success");
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<InteractionGetResponse>> findAllOfUser(
+            @PathVariable UUID userId,
+            @RequestParam(required = false) InteractionType type,
+            @RequestParam(required = false) OffsetDateTime from,
+            @RequestParam(required = false) OffsetDateTime to,
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize
+    ) {
+        List<InteractionGetResponse> response = engagementService.findAllOfUser(
+                userId, type, from, to, pageNumber, pageSize
+        );
+
+        return ResponseEntity.ok(response);
     }
 
 }
