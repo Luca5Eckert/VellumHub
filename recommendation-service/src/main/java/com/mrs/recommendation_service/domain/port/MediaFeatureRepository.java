88b1dde4a21dc9418eb20960572b30c61adeddd9
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-05 15:39:27 -0300

    #54 feat: add vector similarity query in JpaMediaFeatureRepository

--- recommendation-service/src/main/java/com/mrs/recommendation_service/domain/port/MediaFeatureRepository.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/domain/port/MediaFeatureRepository.java
@@ -1,7 +1,10 @@
 package com.mrs.recommendation_service.domain.port;
 
 import com.mrs.recommendation_service.domain.model.MediaFeature;
+import com.mrs.recommendation_service.domain.model.Recommendation;
+import com.mrs.recommendation_service.domain.model.UserProfile;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,6 +12,8 @@ public interface MediaFeatureRepository{
     void save(MediaFeature mediaFeature);
 
     void deleteById(UUID uuid);
+
+    List<MediaFeature> findAllByUserProfile(UserProfile userProfile);
 
     Optional<MediaFeature> findById(UUID uuid);
 }
--- recommendation-service/src/main/java/com/mrs/recommendation_service/infrastructure/repository/media_feature/JpaMediaFeatureRepository.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/infrastructure/repository/media_feature/JpaMediaFeatureRepository.java
@@ -2,10 +2,23 @@ package com.mrs.recommendation_service.infrastructure.repository.media_feature;
 
 import com.mrs.recommendation_service.domain.model.MediaFeature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JpaMediaFeatureRepository extends JpaRepository<MediaFeature, UUID> {
+
+    @Query(value = """
+        SELECT m.media_id 
+        FROM media_features m
+        JOIN user_profiles u ON u.user_id = :userId
+        WHERE m.media_id NOT IN (SELECT unnest(u.interacted_media_ids))
+        ORDER BY (m.embedding <=> u.profile_vector) * 0.7 + (m.popularity_score * 0.3) ASC
+        LIMIT :limit OFFSET :offset
+        """, nativeQuery = true)
+    List<UUID> findTopVectorRecommendations(UUID userId, int limit, int offset);
+
 }
