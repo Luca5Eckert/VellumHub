78c31d7b761aae24750541583e85e8779c5f1469
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-06 19:02:19 -0300

    feat(catalog-service): add BookRequest entity

---
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_request/domain/BookRequest.java
@@ -0,0 +1,72 @@
+package com.mrs.catalog_service.module.book_request.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "books_request")
+@Getter
+public class BookRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(
+            nullable = false,
+            name = "release_year"
+    )
+    private int releaseYear;
+
+    @Column(name = "cover_url")
+    private String coverUrl;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "page_count")
+    private int pageCount;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Version
+    private long version;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    private Instant deletedAt;
+
+    public BookRequest() {
+    }
+
+    public BookRequest(String title, String description, int releaseYear, String coverUrl, String author, String isbn, int pageCount, String publisher) {
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.coverUrl = coverUrl;
+        this.author = author;
+        this.isbn = isbn;
+        this.pageCount = pageCount;
+        this.publisher = publisher;
+    }
+
+}
