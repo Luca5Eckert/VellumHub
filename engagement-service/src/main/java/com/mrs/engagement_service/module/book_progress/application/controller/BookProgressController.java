8ef5f93e431f212f27338abcea1189b17abba80b
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-09 19:10:27 -0300

    #76 feat(engagement-service): add endpoint to define book status

--- engagement-service/src/main/java/com/mrs/engagement_service/module/book_progress/application/controller/BookProgressController.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/module/book_progress/application/controller/BookProgressController.java
@@ -1,9 +1,13 @@
 package com.mrs.engagement_service.module.book_progress.application.controller;
 
+import com.mrs.engagement_service.infrastructure.service.AuthenticationService;
 import com.mrs.engagement_service.module.book_progress.application.dto.BookProgressResponse;
 import com.mrs.engagement_service.module.book_progress.application.dto.BookStatusRequest;
+import com.mrs.engagement_service.module.book_progress.application.handler.DefineBookStatusHandler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +21,32 @@ import java.util.UUID;
 )
 public class BookProgressController {
 
+    private final DefineBookStatusHandler defineBookStatusHandler;
+
+    private final AuthenticationService authenticationService;
+
+    public BookProgressController(DefineBookStatusHandler defineBookStatusHandler, AuthenticationService authenticationService) {
+        this.defineBookStatusHandler = defineBookStatusHandler;
+        this.authenticationService = authenticationService;
+    }
+
 
     @PostMapping("/{bookId}/status")
     public ResponseEntity<BookProgressResponse> defineBookStatus(
             @PathVariable(value="bookId") UUID bookId,
             @Valid BookStatusRequest request
     ) {
-        return ResponseEntity.ok(null);
+        UUID userId = authenticationService.getAuthenticatedUserId();
+
+        BookProgressResponse response = defineBookStatusHandler.handle(
+                request,
+                userId,
+                bookId
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 
     @PutMapping("/{bookId}/progress")
