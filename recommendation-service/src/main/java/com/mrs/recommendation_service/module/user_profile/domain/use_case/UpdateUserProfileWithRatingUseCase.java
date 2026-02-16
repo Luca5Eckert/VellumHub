2004a4052beaaaf82215cfc2e5c4831735bd71d6
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-16 18:24:19 -0300

    #74 feat: add UpdateUserProfileWithRatingUseCase

--- recommendation-service/src/main/java/com/mrs/recommendation_service/module/user_profile/domain/use_case/UpdateUserProfileWithRatingUseCase.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/module/user_profile/domain/use_case/UpdateUserProfileWithRatingUseCase.java
@@ -1,4 +1,26 @@
 package com.mrs.recommendation_service.module.user_profile.domain.use_case;
 
+import com.mrs.recommendation_service.module.user_profile.domain.command.UpdateUserProfileWithRatingCommand;
+import com.mrs.recommendation_service.module.user_profile.domain.model.UserProfile;
+import com.mrs.recommendation_service.module.user_profile.domain.port.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateUserProfileWithRatingUseCase {
+
+    private final UserProfileRepository repository;
+
+    @Transactional
+    public void execute(UpdateUserProfileWithRatingCommand command) {
+        UserProfile profile = repository.findById(command.userId())
+                .orElseGet(() -> new UserProfile(command.userId()));
+
+        profile.updateScoreByRating(command);
+
+        repository.save(profile);
+    }
+
 }
