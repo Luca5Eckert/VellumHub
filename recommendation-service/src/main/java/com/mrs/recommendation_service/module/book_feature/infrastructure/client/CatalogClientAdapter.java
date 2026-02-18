d07ae538bd47e9c9af79d93e68334cbb9663f208
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-18 20:39:41 -0300

    fix(recommendation-service): change to get mapping

--- recommendation-service/src/main/java/com/mrs/recommendation_service/module/book_feature/infrastructure/client/CatalogClientAdapter.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/module/book_feature/infrastructure/client/CatalogClientAdapter.java
@@ -3,6 +3,7 @@ package com.mrs.recommendation_service.module.book_feature.infrastructure.client
 import com.mrs.recommendation_service.module.book_feature.domain.port.CatalogClient;
 import com.mrs.recommendation_service.module.recommendation.domain.model.Recommendation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +13,7 @@ import java.util.UUID;
 @FeignClient(name = "catalog-service", url = "${catalog-service.url}")
 public interface CatalogClientAdapter extends CatalogClient {
 
-    @PostMapping("/api/book/bulk")
+    @GetMapping("/books/bulk")
     List<Recommendation> fetchRecommendationsBatch(@RequestBody List<UUID> ids);
 
 }
