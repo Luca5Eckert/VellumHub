87248126ca686ef0fd83b758fb4e1730da5b92dd
Luca5Eckert <ecklukatv@gmail.com>
2026-02-18 01:06:25 -0300

    fix(user-service): revert changes

--- user-service/src/main/java/com/mrs/user_service/share/security/config/SecurityConfig.java
+++ user-service/src/main/java/com/mrs/user_service/share/security/config/SecurityConfig.java
@@ -84,8 +84,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager() {
-        var authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(new UserDetailsServiceImpl(userRepository));
+        var authProvider = new DaoAuthenticationProvider((new UserDetailsServiceImpl(userRepository)));
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return new ProviderManager(authProvider);
