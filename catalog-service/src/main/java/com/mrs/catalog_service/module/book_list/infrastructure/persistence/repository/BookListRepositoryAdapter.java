0d44fa6a630d67efffc9ddc0fe79ba2ea42aba54
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 17:00:07 -0300

    #123 fix: remove useless imports

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/infrastructure/persistence/repository/BookListRepositoryAdapter.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/infrastructure/persistence/repository/BookListRepositoryAdapter.java
@@ -8,7 +8,6 @@ import com.mrs.catalog_service.module.book_list.infrastructure.persistence.speci
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/presentation/controller/BookListController.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/presentation/controller/BookListController.java
@@ -15,11 +15,9 @@ import com.mrs.catalog_service.module.book_list.presentation.dto.request.UpdateB
 import com.mrs.catalog_service.module.book_list.presentation.mapper.BookListMapper;
 import com.mrs.catalog_service.share.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
