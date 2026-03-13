582f239d24d58283037f883daf8b635a5733eeae
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 20:49:15 -0300

    #131 refactor(recommendation-service): change recommendation to be an entity

--- recommendation-service/src/main/java/com/mrs/recommendation_service/module/recommendation/domain/model/Recommendation.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/module/recommendation/domain/model/Recommendation.java
@@ -1,19 +1,58 @@
 package com.mrs.recommendation_service.module.recommendation.domain.model;
 
 import com.mrs.recommendation_service.module.book_feature.domain.model.Genre;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-public record Recommendation(
-        UUID id,
-        String title,
-        String description,
-        int releaseYear,
-        String coverUrl,
-        List<Genre> genres,
-        Instant createAt,
-        Instant updateAt
-) {
+@Entity
+@Table(
+        name ="recommendations"
+)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Recommendation {
+
+    @Id
+    private UUID bookId;
+
+    @Column(
+            nullable = false
+    )
+    private String title;
+
+    @Column(
+            nullable = false
+    )
+    private String description;
+
+    @Column(
+            nullable = false
+    )
+    private int releaseYear;
+
+    @Column(
+            nullable = false
+    )
+    private String coverUrl;
+
+    @Column(
+            nullable = false
+    )
+    private String author;
+
+    @Column(
+            nullable = false
+    )
+    private List<Genre> genres;
+
 }
