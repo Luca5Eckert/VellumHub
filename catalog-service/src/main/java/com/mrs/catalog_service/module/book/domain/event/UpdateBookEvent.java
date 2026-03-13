3032d1e23d78e3254d9f159b0c087b82e5d21ca2
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 19:53:14 -0300

    #130 refactor: add attributes in UpdateBookEvent for event-query

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book/domain/event/UpdateBookEvent.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book/domain/event/UpdateBookEvent.java
@@ -7,6 +7,12 @@ import java.util.UUID;
 
 public record UpdateBookEvent(
         UUID bookId,
+        String title,
+        String description,
+        int releaseYear,
+        String coverUrl,
+        String author,
         List<Genre> genres
+
 ) {
 }
