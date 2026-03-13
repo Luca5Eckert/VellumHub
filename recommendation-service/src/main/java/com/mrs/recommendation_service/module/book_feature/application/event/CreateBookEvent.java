b82cb8ff4db1933c4dcf5c98841b865f3de20c03
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 20:44:34 -0300

    #130 refactor(recommendation-service): add attributes in events of books-feature

--- recommendation-service/src/main/java/com/mrs/recommendation_service/module/book_feature/application/event/CreateBookEvent.java
+++ recommendation-service/src/main/java/com/mrs/recommendation_service/module/book_feature/application/event/CreateBookEvent.java
@@ -5,8 +5,13 @@ import com.mrs.recommendation_service.module.book_feature.domain.model.Genre;
 import java.util.List;
 import java.util.UUID;
 
-public record CreateBookEvent (
+public record CreateBookEvent(
         UUID bookId,
+        String title,
+        String description,
+        int releaseYear,
+        String coverUrl,
+        String author,
         List<Genre> genres
 ) {
 }
