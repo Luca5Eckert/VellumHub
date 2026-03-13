b7fbb665836cef90273d8f1d9ebe29d14dcab85a
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 16:01:00 -0300

    #123 feat: add GetAllBookListUseCase

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/query/GetAllBookListQuery.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/query/GetAllBookListQuery.java
@@ -1,6 +1,20 @@
 package com.mrs.catalog_service.module.book_list.application.query;
 
-public record GetAllBookListQuery(
+import com.mrs.catalog_service.module.book.domain.model.Genre;
+import com.mrs.catalog_service.module.book_list.domain.model.TypeBookList;
 
+import java.util.Set;
+import java.util.UUID;
+
+public record GetAllBookListQuery(
+        String title,
+        String description,
+        UUID userOwnerList,
+        Set<Genre> genres,
+        Set<UUID> booksId,
+        TypeBookList typeBookList,
+        UUID userAuthenticated,
+        int pageNumber,
+        int pageSize
 ) {
 }
