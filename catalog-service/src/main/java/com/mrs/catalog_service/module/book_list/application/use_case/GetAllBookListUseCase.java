ea6e18814a8be0c342f5d59cc3cfb37c7c67bdd8
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-13 16:01:00 -0300

    #123 feat: add GetAllBookListUseCase

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/use_case/GetAllBookListUseCase.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/use_case/GetAllBookListUseCase.java
@@ -1,4 +1,36 @@
 package com.mrs.catalog_service.module.book_list.application.use_case;
 
+import com.mrs.catalog_service.module.book_list.application.query.GetAllBookListQuery;
+import com.mrs.catalog_service.module.book_list.domain.filter.BookListFilter;
+import com.mrs.catalog_service.module.book_list.domain.filter.BookListPage;
+import com.mrs.catalog_service.module.book_list.domain.model.BookList;
+import com.mrs.catalog_service.module.book_list.domain.port.BookListRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GetAllBookListUseCase {
+
+    private final BookListRepository bookListRepository;
+
+    public GetAllBookListUseCase(BookListRepository bookListRepository) {
+        this.bookListRepository = bookListRepository;
+    }
+
+    public Page<BookList> execute(GetAllBookListQuery query){
+        BookListFilter bookListFilter = BookListFilter.of(
+                query.title(),
+                query.description(),
+                query.userOwnerList(),
+                query.genres(),
+                query.booksId(),
+                query.typeBookList(),
+                query.userAuthenticated()
+        );
+        BookListPage bookListPage = BookListPage.of(query.pageNumber(), query.pageSize());
+
+        return bookListRepository.findAll(bookListFilter, bookListPage);
+    }
 }
