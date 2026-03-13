1d976dac6bd3957b8a7c28f2cc1a724f46e9d83c
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-12 21:48:00 -0300

    feat: add GetAllBookListUseCase

---
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/query/GetAllBookListQuery.java
@@ -0,0 +1,6 @@
+package com.mrs.catalog_service.module.book_list.application.query;
+
+public record GetAllBookListQuery(
+
+) {
+}
---
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/use_case/GetAllBookListUseCase.java
@@ -0,0 +1,4 @@
+package com.mrs.catalog_service.module.book_list.application.use_case;
+
+public class GetAllBookListUseCase {
+}
