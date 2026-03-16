2255fecae1606449e6f4a843fa4cfeafdabe0663
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-16 16:59:23 -0300

    #136 fix: fix grammar error message

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/use_case/member/GetAllMembershipsOfBookListUseCase.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book_list/application/use_case/member/GetAllMembershipsOfBookListUseCase.java
@@ -22,14 +22,14 @@ public class GetAllMembershipsOfBookListUseCase {
      * Check if the user has permission to read the book list
      * @param query the query containing the information needed to get all memberships of a book list
      * @return a list of memberships of the book list
-     * @throws BookListDomainException if the book list is not found or if the user don't have permission to read the book list
+     * @throws BookListDomainException if the book list is not found or if the user doesn't have permission to read the book list
      */
     public List<BookListMembership> execute(GetAllMembershipOfBookListQuery query) {
         var bookList = bookListRepository.findById(query.bookListId())
                 .orElseThrow(() -> new BookListDomainException("Book list not found"));
 
         if(!bookList.canRead(query.userAuthenticatedId())) {
-            throw new BookListDomainException("User don't have permission to read this book list");
+            throw new BookListDomainException("User doesn't have permission to read this book list");
         }
 
         return bookList.getMemberships();
