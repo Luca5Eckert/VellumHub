d20a0309c6842aafeace98683cb9a7171f1980ad
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-20 21:50:45 -0300

    #146 fix: implement GenreRepository

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book/infrastructure/repository/book/JpaBookRepository.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book/infrastructure/repository/book/JpaBookRepository.java
@@ -1,4 +1,4 @@
-package com.mrs.catalog_service.module.book.infrastructure.repository;
+package com.mrs.catalog_service.module.book.infrastructure.repository.book;
 
 import com.mrs.catalog_service.module.book.domain.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
