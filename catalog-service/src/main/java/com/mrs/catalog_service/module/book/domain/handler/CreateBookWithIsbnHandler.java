5c2bffd4ce7d32d51d1b3b81fc33e722fda37bf1
Luca5Eckert <ecklukatv@gmail.com>
2026-03-25 12:07:17 -0300

    #143 fix: add publication of created book event in CreateBookWithIsbnHandler

--- catalog-service/src/main/java/com/mrs/catalog_service/module/book/domain/handler/CreateBookWithIsbnHandler.java
+++ catalog-service/src/main/java/com/mrs/catalog_service/module/book/domain/handler/CreateBookWithIsbnHandler.java
@@ -1,7 +1,10 @@
 package com.mrs.catalog_service.module.book.domain.handler;
 
+import com.mrs.catalog_service.module.book.domain.event.CreateBookEvent;
 import com.mrs.catalog_service.module.book.domain.exception.BookDomainException;
 import com.mrs.catalog_service.module.book.domain.model.Book;
+import com.mrs.catalog_service.module.book.domain.model.Genre;
+import com.mrs.catalog_service.module.book.domain.port.BookEventProducer;
 import com.mrs.catalog_service.module.book.domain.port.BookExternalProvider;
 import com.mrs.catalog_service.module.book.domain.port.BookRepository;
 import org.springframework.stereotype.Component;
@@ -15,9 +18,13 @@ public class CreateBookWithIsbnHandler {
     private final BookRepository bookRepository;
     private final BookExternalProvider bookExternalProvider;
 
-    public CreateBookWithIsbnHandler(BookRepository bookRepository, BookExternalProvider bookExternalProvider) {
+    private final BookEventProducer<String, CreateBookEvent> bookEventProducer;
+
+
+    public CreateBookWithIsbnHandler(BookRepository bookRepository, BookExternalProvider bookExternalProvider, BookEventProducer<String, CreateBookEvent> bookEventProducer) {
         this.bookRepository = bookRepository;
         this.bookExternalProvider = bookExternalProvider;
+        this.bookEventProducer = bookEventProducer;
     }
 
     @Transactional
@@ -31,6 +38,28 @@ public class CreateBookWithIsbnHandler {
                 .orElseThrow(() -> new BookDomainException("Book not found in external provider for ISBN: " + isbn));
 
         bookRepository.save(newBook);
+
+
+
         return newBook.getId();
     }
+
+    /**
+     * Publishes a CreateBookEvent to notify other parts of the system about the creation of a new book.
+     * @param book The book that was created, used to populate the event data
+     */
+    private void publishEvent(Book book) {
+        CreateBookEvent createBookEvent = new CreateBookEvent(
+                book.getId(),
+                book.getTitle(),
+                book.getDescription(),
+                book.getReleaseYear(),
+                book.getCoverUrl(),
+                book.getAuthor(),
+                book.getGenres().stream().map(Genre::getName).toList()
+        );
+
+        bookEventProducer.send("created-book", createBookEvent.bookId().toString(), createBookEvent);
+    }
+
 }
