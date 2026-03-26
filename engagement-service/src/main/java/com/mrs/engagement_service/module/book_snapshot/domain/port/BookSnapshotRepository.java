ec3da69e5af26a5c0d6c01a13c5283ff1a5a714f
Luca5Eckert <ecklukatv@gmail.com>
2026-03-26 12:36:27 -0300

    #154 feat: add DeleteBookSnapshotUseCase

--- engagement-service/src/main/java/com/mrs/engagement_service/module/book_snapshot/domain/port/BookSnapshotRepository.java
+++ engagement-service/src/main/java/com/mrs/engagement_service/module/book_snapshot/domain/port/BookSnapshotRepository.java
@@ -2,6 +2,12 @@ package com.mrs.engagement_service.module.book_snapshot.domain.port;
 
 import com.mrs.engagement_service.module.book_snapshot.domain.model.BookSnapshot;
 
+import java.util.UUID;
+
 public interface BookSnapshotRepository {
     void save(BookSnapshot bookSnapshot);
+
+    boolean existsByBookId(UUID bookId);
+
+    void deleteByBookId(UUID uuid);
 }
