16a7e9b0182160981c2e9f012c6f8e61fb5d64a3
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-27 15:37:48 -0300

    fix: add Component to SpringGenreRepositoryAdapter

--- catalog-service/src/main/java/com/vellumhub/catalog_service/module/book/infrastructure/repository/genre/SpringGenreRepositoryAdapter.java
+++ catalog-service/src/main/java/com/vellumhub/catalog_service/module/book/infrastructure/repository/genre/SpringGenreRepositoryAdapter.java
@@ -29,4 +29,5 @@ public class SpringGenreRepositoryAdapter implements GenreRepository {
     public boolean existsByName(String name) {
         return jpaGenreRepository.existsByName(name);
     }
+
 }
