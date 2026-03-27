d077da8c43638b5f4708f9e3f4781037e30c69ee
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-27 20:14:40 -0300

    fix: update request mapping path for RecommendationController

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/recommendation/presentation/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/recommendation/presentation/controller/RecommendationController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/recommendations")
+@RequestMapping("/recommendations")
 @Tag(name = "Recommendations", description = "Endpoints for obtaining personalized book recommendations")
 public class RecommendationController {
 
