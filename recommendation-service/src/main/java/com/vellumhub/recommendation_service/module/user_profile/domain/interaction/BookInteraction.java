d423022fdcaaa4ed9a3c84281b492bdae3d0fc79
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-27 21:37:17 -0300

    #168 refactor: introduce RatingBookInteraction to handle book rating adjustments

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/BookInteraction.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/BookInteraction.java
@@ -1,8 +1,12 @@
 package com.vellumhub.recommendation_service.module.user_profile.domain.interaction;
 
+import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
 import com.vellumhub.recommendation_service.module.user_profile.domain.model.ProfileAdjustment;
+
+import java.util.UUID;
 
 public interface BookInteraction
 {
-    ProfileAdjustment toAdjustment();
+    public ProfileAdjustment toAdjustment(BookFeature bookFeature);
+
 }
