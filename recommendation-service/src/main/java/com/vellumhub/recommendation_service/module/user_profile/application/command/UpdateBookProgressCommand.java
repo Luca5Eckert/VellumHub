b50507c9bc1d3d39240c7b5b09ba39cf80aee4db
Luca5Eckert <ecklukatv@gmail.com>
2026-03-28 19:45:27 -0300

    #170 feat: extend UpdateBookProgressCommand and UpdateBookProgressEvent to include oldPage and newPage

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/command/UpdateBookProgressCommand.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/command/UpdateBookProgressCommand.java
@@ -6,9 +6,17 @@ import java.util.UUID;
 public record UpdateBookProgressCommand(
         UUID userId,
         UUID bookId,
-        String progress
+        String progress,
+        int oldPage,
+        int newPage
 ) {
-    public static UpdateBookProgressCommand of(UUID userId, UUID bookId, String progress) {
-        return new UpdateBookProgressCommand(userId, bookId, progress);
+    public static UpdateBookProgressCommand of(
+            UUID userId,
+            UUID bookId,
+            String progress,
+            int oldPage,
+            int newPage
+    ) {
+        return new UpdateBookProgressCommand(userId, bookId, progress,oldPage, newPage);
     }
 }
