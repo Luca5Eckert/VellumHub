ad67d0ec51eab4e74911d8f64c04206d3d3e65be
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-27 21:37:17 -0300

    #168 refactor: introduce RatingBookInteraction to handle book rating adjustments

---
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/RatingBookInteration.java
@@ -0,0 +1,43 @@
+package com.vellumhub.recommendation_service.module.user_profile.domain.interaction;
+
+import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
+import com.vellumhub.recommendation_service.module.user_profile.domain.model.ProfileAdjustment;
+import com.vellumhub.recommendation_service.module.user_profile.domain.model.RatingCategory;
+
+
+public class RatingBookInteration implements BookInteraction{
+
+    private final int oldStars;
+    private final int newStars;
+    private final boolean isNewRating;
+
+    public RatingBookInteration(int oldStars, int newStars, boolean isNewRating) {
+        this.oldStars = oldStars;
+        this.newStars = newStars;
+        this.isNewRating = isNewRating;
+    }
+
+    @Override
+    public ProfileAdjustment toAdjustment(BookFeature bookFeature) {
+        if (!hasCategoryChanged(newStars, oldStars, isNewRating)) {
+            return ProfileAdjustment.of(bookFeature.getBookId(), 0, bookFeature.getEmbedding());
+        }
+
+        int adjustmentWeight = getWeightAdjustment(newStars, oldStars, isNewRating);
+
+        return ProfileAdjustment.of(bookFeature.getBookId(), adjustmentWeight, bookFeature.getEmbedding());
+    }
+
+    private int getWeightAdjustment(int newStars, int oldStars, boolean isNewRating) {
+        int newWeight = RatingCategory.fromStars(newStars).getWeight();
+        int oldWeight = isNewRating ? 0 : RatingCategory.fromStars(oldStars).getWeight();
+
+        return newWeight - oldWeight;
+    }
+
+    private boolean hasCategoryChanged(int newStars, int oldStars, boolean isNewRating) {
+        if (isNewRating) return true;
+        return RatingCategory.fromStars(oldStars) != RatingCategory.fromStars(newStars);
+    }
+
+}
--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/model/ProfileAdjustment.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/model/ProfileAdjustment.java
@@ -3,8 +3,13 @@ package com.vellumhub.recommendation_service.module.user_profile.domain.model;
 import java.util.UUID;
 
 public record ProfileAdjustment(
-        UUID userId,
+        UUID bookId,
         float adjustment,
         float[] embedding
 ) {
+
+    public static ProfileAdjustment of(UUID bookId, float adjustment, float[] embedding) {
+        return new ProfileAdjustment(bookId, adjustment, embedding);
+    }
+
 }
