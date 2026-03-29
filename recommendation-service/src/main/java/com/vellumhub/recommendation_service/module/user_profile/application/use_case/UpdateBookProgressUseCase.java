3c7d21edd92b8d52865bee0c113ff448f8dc5a8f
Luca5Eckert <ecklukatv@gmail.com>
2026-03-28 23:49:20 -0300

    #170 chore: add javadoc in UpdateBookProgressUseCase

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/use_case/UpdateBookProgressUseCase.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/use_case/UpdateBookProgressUseCase.java
@@ -3,6 +3,10 @@ package com.vellumhub.recommendation_service.module.user_profile.application.use
 import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
 import com.vellumhub.recommendation_service.module.book_feature.domain.port.BookFeatureRepository;
 import com.vellumhub.recommendation_service.module.user_profile.application.command.UpdateBookProgressCommand;
+import com.vellumhub.recommendation_service.module.user_profile.domain.interaction.BookInteraction;
+import com.vellumhub.recommendation_service.module.user_profile.domain.interaction.BookProgressInteraction;
+import com.vellumhub.recommendation_service.module.user_profile.domain.interaction.RatingBookInteration;
+import com.vellumhub.recommendation_service.module.user_profile.domain.model.ProfileAdjustment;
 import com.vellumhub.recommendation_service.module.user_profile.domain.model.UserProfile;
 import com.vellumhub.recommendation_service.module.user_profile.domain.port.UserProfileRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +22,10 @@ public class UpdateBookProgressUseCase {
         this.bookFeatureRepository = bookFeatureRepository;
     }
 
+    /**
+     * Updates the user's profile based on their progress with a book. It retrieves the user's profile and the book's features,
+     * @param command the command containing the data for updating the book progress.
+     */
     public void execute(UpdateBookProgressCommand command){
         UserProfile profile = userProfileRepository.findById(command.userId())
                 .orElseGet(() -> new UserProfile(command.userId()));
@@ -25,7 +33,33 @@ public class UpdateBookProgressUseCase {
         BookFeature book = bookFeatureRepository.findById(command.bookId())
                 .orElseThrow(() -> new RuntimeException("Book features not found"));
 
+        ProfileAdjustment profileAdjustment = getProfileAdjustment(
+                command.oldPage(),
+                command.newPage(),
+                command.progress(),
+                book
+        );
 
+        profile.applyUpdate(profileAdjustment);
+    }
+
+    /**
+     * Calculates the profile adjustment based on the user's progress with the book.
+     * @param oldPages the number of pages the user had read before the update.
+     * @param newPages the number of pages the user has read after the update.
+     * @param progress the current progress status of the book (e.g., "WANT_TO_READ", "READING", "COMPLETED").
+     * @param book the book feature data used to calculate the profile adjustment.
+     * @return a ProfileAdjustment object that represents the changes to be applied to the user's profile based on their book progress.
+     */
+    private ProfileAdjustment getProfileAdjustment(
+            int oldPages,
+            int newPages,
+            String progress,
+            BookFeature book
+    ) {
+        BookInteraction bookInteraction = new BookProgressInteraction(progress, oldPages, newPages);
+
+        return bookInteraction.toAdjustment(book);
     }
 
 }
