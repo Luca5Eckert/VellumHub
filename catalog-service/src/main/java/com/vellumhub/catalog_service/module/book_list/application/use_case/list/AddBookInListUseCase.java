8a4ee515b8e05fcc9d183349ea5a75eb673a45e5
Luca5Eckert <ecklukatv@gmail.com>
2026-03-31 00:10:21 -0300

    #124 feat: add use case and command for adding a book to a book list

---
+++ catalog-service/src/main/java/com/vellumhub/catalog_service/module/book_list/application/use_case/list/AddBookInListUseCase.java
@@ -0,0 +1,32 @@
+package com.vellumhub.catalog_service.module.book_list.application.use_case.list;
+
+import com.vellumhub.catalog_service.module.book.domain.port.BookRepository;
+import com.vellumhub.catalog_service.module.book_list.application.command.list.AddBookInListCommand;
+import com.vellumhub.catalog_service.module.book_list.domain.port.BookListRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AddBookInListUseCase {
+
+    private final BookListRepository bookListRepository;
+    private final BookRepository bookRepository;
+
+    public AddBookInListUseCase(BookListRepository bookListRepository, BookRepository bookRepository) {
+        this.bookListRepository = bookListRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public void execute(AddBookInListCommand command){
+        var bookList = bookListRepository.findById(command.listId())
+                .orElseThrow(() -> new RuntimeException("Book list not found"));
+
+        var book = bookRepository.findById(command.bookId())
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        bookList.addBook(book);
+
+        bookListRepository.save(bookList);
+    }
+}
