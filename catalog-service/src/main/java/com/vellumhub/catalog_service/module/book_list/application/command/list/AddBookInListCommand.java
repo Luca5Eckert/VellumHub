9d418ce28d57246d478071bef1d5cb047086c17e
Luca5Eckert <ecklukatv@gmail.com>
2026-03-31 00:12:20 -0300

    #124 feat: add userId to AddBookInListCommand and provide a static factory method

--- catalog-service/src/main/java/com/vellumhub/catalog_service/module/book_list/application/command/list/AddBookInListCommand.java
+++ catalog-service/src/main/java/com/vellumhub/catalog_service/module/book_list/application/command/list/AddBookInListCommand.java
@@ -3,7 +3,11 @@ package com.vellumhub.catalog_service.module.book_list.application.command.list;
 import java.util.UUID;
 
 public record AddBookInListCommand(
+        UUID userId,
         UUID listId,
         UUID bookId
 ) {
+    public static AddBookInListCommand of(UUID userId, UUID bookListId, UUID bookId) {
+        return new AddBookInListCommand(userId, bookListId, bookId);
+    }
 }
