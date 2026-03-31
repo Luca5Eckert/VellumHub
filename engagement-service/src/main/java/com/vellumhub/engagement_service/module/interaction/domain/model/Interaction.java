442d3abddb31a06796495eabd75c69b62dafe041
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-03-31 18:36:45 -0300

    #152 feat: enhance Interaction model with new fields and static factory method

--- engagement-service/src/main/java/com/vellumhub/engagement_service/module/interaction/domain/model/Interaction.java
+++ engagement-service/src/main/java/com/vellumhub/engagement_service/module/interaction/domain/model/Interaction.java
@@ -1,6 +1,43 @@
 package com.vellumhub.engagement_service.module.interaction.domain.model;
 
+import com.vellumhub.engagement_service.module.book_snapshot.domain.model.BookSnapshot;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "interactions")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Interaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "interaction_gen")
+    @SequenceGenerator(name = "interaction_gen", sequenceName = "interactions_seq", allocationSize = 1)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_snapshot_id")
+    private BookSnapshot bookSnapshot;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TypeInteraction typeInteraction;
+
+    public static Interaction of(UUID userId, BookSnapshot snapshot, TypeInteraction type) {
+        return Interaction.builder()
+                .userId(userId)
+                .bookSnapshot(snapshot)
+                .typeInteraction(type)
+                .build();
+    }
 
 }
--- engagement-service/src/main/java/com/vellumhub/engagement_service/module/interaction/domain/model/TypeInteraction.java
+++ engagement-service/src/main/java/com/vellumhub/engagement_service/module/interaction/domain/model/TypeInteraction.java
@@ -1,6 +1,7 @@
 package com.vellumhub.engagement_service.module.interaction.domain.model;
 
 public enum TypeInteraction {
-    LIKE,
-    NOT_INTERESTED
+    VERY_POSITIVE,
+    POSITIVE,
+    NEGATIVE
 }
