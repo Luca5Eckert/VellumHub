33f343902fc637f1120c8df4e3958b556d9e8fec
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-01 20:39:49 -0300

    #177 refactor: change BookProgressInteraction to be a component

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/progress/BookProgressInteraction.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/progress/BookProgressInteraction.java
@@ -1,26 +1,26 @@
 package com.vellumhub.recommendation_service.module.user_profile.domain.interaction.progress;
 
 import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
-import com.vellumhub.recommendation_service.module.user_profile.domain.interaction.BookInteraction;
 import com.vellumhub.recommendation_service.module.user_profile.domain.model.ProfileAdjustment;
+import org.springframework.stereotype.Service;
 
-public class BookProgressInteraction implements BookInteraction {
+@Service
+public class BookProgressInteraction  {
 
-    private final String progress;
-    private final int oldPage;
-    private final int newPage;
+    private final static float PAGE_PROGRESS_WEIGHT = 0.0125f;
 
-    public BookProgressInteraction(String progress, int oldPage, int newPage) {
-        this.progress = progress;
-        this.oldPage = oldPage;
-        this.newPage = newPage;
-    }
+    /**
+     * Calculates the profile adjustment based on the user's progress in reading a book.
+     * @param bookFeature The features of the book being read.
+     * @param progress The progress type.
+     * @param oldPage The page number before the progress update.
+     * @param newPage The page number after the progress update.
+     * @return  A ProfileAdjustment object containing the calculated adjustment for the user's profile based on the progress and the book features.
+     */
+    public ProfileAdjustment toAdjustment(BookFeature bookFeature, String progress, int oldPage, int newPage) {
+        float adjustment = perTypeProgress(progress);
 
-    @Override
-    public ProfileAdjustment toAdjustment(BookFeature bookFeature) {
-        float adjustment = perTypeProgress();
-
-        adjustment += calculateScorePerProgress();
+        adjustment += calculateScorePerProgress(oldPage, newPage);
 
         return new ProfileAdjustment(
                 bookFeature.getBookId(),
@@ -29,18 +29,24 @@ public class BookProgressInteraction implements BookInteraction {
         );
     }
 
-    private float perTypeProgress() {
-        return switch(progress) {
-            case "WANT_TO_READ" -> 0.5f;
-            case "READING" -> 1.0f;
-            case "COMPLETED" -> 2.0f;
-            default -> 0.0f;
-        };
+    /**
+     * Calculates the adjustment value based on the type of progress.
+     * @param progress The type of progress.
+     * @return A float value representing the adjustment.
+     */
+    private float perTypeProgress(String progress) {
+        return Progress.of(progress).adjusment;
     }
 
-    private float calculateScorePerProgress() {
+    /**
+     * Calculates the score adjustment based on the number of pages read.
+     * @param oldPage The page number before the progress update.
+     * @param newPage The page number after the progress update.
+     * @return A float value representing the score adjustment based on the pages read and the defined weight for page progress.
+     */
+    private float calculateScorePerProgress(int oldPage, int newPage) {
         int readPages = newPage - oldPage;
 
-        return (float) readPages / 0.1f;
+        return (float) readPages * PAGE_PROGRESS_WEIGHT;
     }
 }
