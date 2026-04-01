cede6563b5948e19ff8173c006934664e2e7a904
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-01 20:45:20 -0300

    #177 refactor: simplify RatingBookInteraction by removing interface implementation and adjusting method signature

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/rating/RatingBookInteraction.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/domain/interaction/rating/RatingBookInteraction.java
@@ -1,24 +1,18 @@
 package com.vellumhub.recommendation_service.module.user_profile.domain.interaction.rating;
 
 import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
-import com.vellumhub.recommendation_service.module.user_profile.domain.interaction.BookInteraction;
 import com.vellumhub.recommendation_service.module.user_profile.domain.model.ProfileAdjustment;
+import org.springframework.stereotype.Service;
 
+@Service
+public class RatingBookInteraction {
 
-public class RatingBookInteraction implements BookInteraction {
-
-    private final int oldStars;
-    private final int newStars;
-    private final boolean isNewRating;
-
-    public RatingBookInteraction(int oldStars, int newStars, boolean isNewRating) {
-        this.oldStars = oldStars;
-        this.newStars = newStars;
-        this.isNewRating = isNewRating;
-    }
-
-    @Override
-    public ProfileAdjustment toAdjustment(BookFeature bookFeature) {
+    public ProfileAdjustment toAdjustment(
+            BookFeature bookFeature,
+            int oldStars,
+            int newStars,
+            boolean isNewRating
+    ) {
         if (!hasCategoryChanged(newStars, oldStars, isNewRating)) {
             return ProfileAdjustment.of(bookFeature.getBookId(), 0, bookFeature.getEmbedding());
         }
