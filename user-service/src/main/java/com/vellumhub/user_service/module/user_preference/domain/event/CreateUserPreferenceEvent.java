a8734becd7f7829dc93d1a400b082abb6be0efb7
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-02 13:53:44 -0300

    #179 feat: update CreateUserPreferenceEvent to use String for genres and add about field

--- user-service/src/main/java/com/vellumhub/user_service/module/user_preference/domain/event/CreateUserPreferenceEvent.java
+++ user-service/src/main/java/com/vellumhub/user_service/module/user_preference/domain/event/CreateUserPreferenceEvent.java
@@ -1,13 +1,12 @@
 package com.vellumhub.user_service.module.user_preference.domain.event;
 
 
-import com.vellumhub.user_service.module.user.domain.Genre;
-
 import java.util.List;
 import java.util.UUID;
 
 public record CreateUserPreferenceEvent (
         UUID userId,
-        List<Genre> genres
+        List<String> genres,
+        String about
 ) {
 }
