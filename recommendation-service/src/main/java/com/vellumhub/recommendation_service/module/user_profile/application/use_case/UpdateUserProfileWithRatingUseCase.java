b8570b62a809e290c957cd26966537722efe920a
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-01 21:00:33 -0300

    #177 refactor: replace RuntimeException with BookFeatureNotFoundException in use cases

--- recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/use_case/UpdateUserProfileWithRatingUseCase.java
+++ recommendation-service/src/main/java/com/vellumhub/recommendation_service/module/user_profile/application/use_case/UpdateUserProfileWithRatingUseCase.java
@@ -1,5 +1,6 @@
 package com.vellumhub.recommendation_service.module.user_profile.application.use_case;
 
+import com.vellumhub.recommendation_service.module.book_feature.domain.exception.BookFeatureNotFoundException;
 import com.vellumhub.recommendation_service.module.book_feature.domain.model.BookFeature;
 import com.vellumhub.recommendation_service.module.book_feature.domain.port.BookFeatureRepository;
 import com.vellumhub.recommendation_service.module.user_profile.application.command.UpdateUserProfileWithRatingCommand;
@@ -28,7 +29,7 @@ public class UpdateUserProfileWithRatingUseCase {
                 .orElseGet(() -> new UserProfile(command.userId()));
 
         BookFeature book = bookFeatureRepository.findById(command.bookId())
-                .orElseThrow(() -> new RuntimeException("Book features not found"));
+                .orElseThrow(() -> new BookFeatureNotFoundException("Book features not found"));
 
         ProfileAdjustment profileAdjustment = ratingBookInteraction.toAdjustment(
                 book,
