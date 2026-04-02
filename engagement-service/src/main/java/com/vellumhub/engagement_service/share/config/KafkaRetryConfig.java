988cad3a0c12758ad4bda9b998db34fd0c6def19
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-02 15:51:35 -0300

    #181 feat(engagement-service): add default retry configuration and DLT listener for Kafka topics

---
+++ engagement-service/src/main/java/com/vellumhub/engagement_service/share/config/KafkaRetryConfig.java
@@ -0,0 +1,63 @@
+package com.vellumhub.engagement_service.share.config;
+
+import org.springframework.messaging.handler.annotation.Header;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.retrytopic.RetryTopicConfiguration;
+import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
+
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class KafkaRetryConfig {
+
+    /**
+     * Defines a default retry configuration for Kafka listeners in the application.
+     * @param template the KafkaTemplate used to send messages to retry topics
+     * @return a RetryTopicConfiguration that applies to all specified topics with a fixed backoff strategy and a maximum of 3 attempts.
+     */
+    @Bean
+    public RetryTopicConfiguration defaultRetryConfig(KafkaTemplate<String, Object> template) {
+        return RetryTopicConfigurationBuilder
+                .newInstance()
+                .maxAttempts(3)
+                .fixedBackOff(3000)
+                .includeTopics(List.of(
+                        "created-book",
+                        "deleted-book"
+                ))
+                .create(template);
+    }
+
+
+    /**
+     * Listens to all Dead Letter Topics across the microservice.
+     * The topicPattern ".*-dlt" ensures any topic ending with "-dlt" is captured here.
+     */
+    @KafkaListener(
+            topicPattern = ".*-dlt",
+            groupId = "recommendation-service-dlt-group"
+    )
+    public void consumeDlt(
+            String payload,
+            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+            @Header(KafkaHeaders.ORIGINAL_TOPIC) String originalTopic,
+            @Header(KafkaHeaders.EXCEPTION_MESSAGE) String errorMessage
+    ) {
+
+        log.error("CRITICAL FAILURE: Unrecoverable message routed to DLT.");
+        log.error("Original Topic  : {}", originalTopic);
+        log.error("DLT Topic       : {}", topic);
+        log.error("Exception Reason: {}", errorMessage);
+        log.error("Message Payload : {}", payload);
+        log.error("=====================================================");
+
+    }
+
+
+}
