5e36832721f45802a8ca09ffd4bb259b1458d8c8
Luca5Eckert <ecklukatv@gmail.com>
2026-04-18 00:38:23 -0300

    #191 feat: add CreateReadingSessionEntryRequest and ReadingSessionEntryController

---
+++ engagement-service/src/main/java/com/vellumhub/engagement_service/module/reading_session_entry/presentation/controller/ReadingSessionEntryController.java
@@ -0,0 +1,30 @@
+package com.vellumhub.engagement_service.module.reading_session_entry.presentation.controller;
+
+import com.vellumhub.engagement_service.module.reading_session_entry.application.command.CreateReadingSessionEntryCommand;
+import com.vellumhub.engagement_service.module.reading_session_entry.application.use_case.CreateReadingSessionEntryUseCase;
+import com.vellumhub.engagement_service.module.reading_session_entry.presentation.dto.CreateReadingSessionEntryRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reading-session-entries")
+public class ReadingSessionEntryController {
+
+    private final CreateReadingSessionEntryUseCase createReadingSessionEntryUseCase;
+
+    public ReadingSessionEntryController(CreateReadingSessionEntryUseCase createReadingSessionEntryUseCase) {
+        this.createReadingSessionEntryUseCase = createReadingSessionEntryUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(CreateReadingSessionEntryRequest request){
+        var command = CreateReadingSessionEntryCommand.create(request.bookId(), request.type());
+
+        createReadingSessionEntryUseCase.execute(command);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
