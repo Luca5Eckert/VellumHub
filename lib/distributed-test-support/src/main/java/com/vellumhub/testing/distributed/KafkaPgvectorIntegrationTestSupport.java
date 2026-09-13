9d7053f3dc87eb7998cad052dbdf3ba134fb7f70
Lucas Eckert <lucasismaeleckert@gmail.com>
2026-09-13 19:13:51 -0300

    test(distributed): add shared Kafka and pgvector harness

---
+++ lib/distributed-test-support/src/main/java/com/vellumhub/testing/distributed/KafkaPgvectorIntegrationTestSupport.java
@@ -0,0 +1,16 @@
+package com.vellumhub.testing.distributed;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class KafkaPgvectorIntegrationTestSupport extends KafkaIntegrationTestSupport {
+
+    @Container
+    protected static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
+            DockerImageName.parse("pgvector/pgvector:pg15").asCompatibleSubstituteFor("postgres")
+    )
+            .withDatabaseName("vellumhub_distributed_test")
+            .withUsername("test")
+            .withPassword("test");
+}
